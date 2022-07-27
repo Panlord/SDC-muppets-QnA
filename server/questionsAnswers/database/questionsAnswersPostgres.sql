5354ee27df2bb0d6115643be7b9ac9b264ea7226
Panlord <adpan.dev@gmail.com>
2022-07-27 11:44:39 -0700

    create indexes for questions and answers

--- server/questionsAnswers/database/questionsAnswersPostgres.sql
+++ server/questionsAnswers/database/questionsAnswersPostgres.sql
@@ -14,8 +14,10 @@ CREATE TABLE IF NOT EXISTS QnASchema.questions (
   reported BOOLEAN DEFAULT false,
   product_id INT,
 
-  FOREIGN KEY (product_id) REFERENCES QnaSchema.products (product_id)
+  FOREIGN KEY (product_id) REFERENCES QnASchema.products (product_id)
 );
+
+CREATE INDEX questions_mostHelpful ON QnASchema.questions (helpfulness DESC NULLS LAST);
 
 CREATE TABLE IF NOT EXISTS QnASchema.answers (
   answer_id INT PRIMARY KEY,
@@ -27,13 +29,15 @@ CREATE TABLE IF NOT EXISTS QnASchema.answers (
   reported BOOLEAN DEFAULT false,
   question_id INT,
 
-  FOREIGN KEY (question_id) REFERENCES QnaSchema.questions (question_id)
+  FOREIGN KEY (question_id) REFERENCES QnASchema.questions (question_id)
 );
+
+CREATE INDEX answers_mostHelpful ON QnASchema.answers (helpfulness DESC NULLS LAST);
 
 CREATE TABLE IF NOT EXISTS QnASchema.photos (
   photo_id SERIAL PRIMARY KEY,
   photo_url VARCHAR(1000) NOT NULL,
   answer_id INT,
 
-  FOREIGN KEY (answer_id) REFERENCES QnaSchema.answers (answer_id)
+  FOREIGN KEY (answer_id) REFERENCES QnASchema.answers (answer_id)
 )
