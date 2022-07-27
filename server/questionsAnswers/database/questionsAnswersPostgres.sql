9c1b58161f6e7b76f5942d69e09bc7d6ce09ce45
Panlord <adpan.dev@gmail.com>
2022-07-27 00:04:23 -0700

    complete Postgres schema

--- server/questionsAnswers/database/questionsAnswersPostgres.sql
+++ server/questionsAnswers/database/questionsAnswersPostgres.sql
@@ -1,27 +1,39 @@
 CREATE SCHEMA QnASchema;
 
 CREATE TABLE IF NOT EXISTS QnASchema.products (
-  product_id INT PRIMARY KEY,
-  question_ids INT[]
+  product_id INT PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS QnASchema.questions (
   question_id INT PRIMARY KEY,
-  question_body VARCHAR(1000),
+  question_body VARCHAR(1000) NOT NULL,
   question_date DATE,
-  asker_name VARCHAR(60),
-  asker_email VARCHAR(60),
-  helpfulness INT,
-  reported BOOLEAN,
-  answer_ids INT[]
+  asker_name VARCHAR(60) NOT NULL,
+  asker_email VARCHAR(60) NOT NULL,
+  helpfulness INT DEFAULT 0,
+  reported BOOLEAN DEFAULT false,
+  product_id INT,
+
+  FOREIGN KEY (product_id) REFERENCES QnaSchema.products (product_id)
 );
 
 CREATE TABLE IF NOT EXISTS QnASchema.answers (
   answer_id INT PRIMARY KEY,
-  answer_body VARCHAR(1000),
+  answer_body VARCHAR(1000) NOT NULL,
   answer_date DATE,
-  answerer_name VARCHAR(60),
-  answerer_email VARCHAR(60),
-  helpfulness INT,
-  photos VARCHAR(1000)[]
+  answerer_name VARCHAR(60) NOT NULL,
+  answerer_email VARCHAR(60) NOT NULL,
+  helpfulness INT DEFAULT 0,
+  reported BOOLEAN DEFAULT false,
+  question_id INT,
+
+  FOREIGN KEY (question_id) REFERENCES QnaSchema.questions (question_id)
 );
+
+CREATE TABLE IF NOT EXISTS QnASchema.photos (
+  photo_id SERIAL PRIMARY KEY,
+  photo_url VARCHAR(1000) NOT NULL,
+  answer_id INT,
+
+  FOREIGN KEY (answer_id) REFERENCES QnaSchema.answers (answer_id)
+)
