72fb4a069cc72fbc58f40a7b0fccda21d28b3afd
Panlord <adpan.dev@gmail.com>
2022-08-01 15:58:35 -0700

    query WIP-- implement queries for all answers of a given question and for a specific given question

--- server/questionsAnswers/database/queryTesting.sql
+++ server/questionsAnswers/database/queryTesting.sql
@@ -10,27 +10,6 @@ WHERE questions.product_id=1
 GROUP BY questions.question_id
 limit 30;
 
-
-/* Does not work */
-SELECT questions.question_id, question_body, asker_name, asker_email, questions.helpfulness, questions.reported, product_id,
-	JSON_BUILD_OBJECT (answer_id, JSON_BUILD_OBJECT (
-		'id', answer_id,
-		'body', answer_body,
-		'date', answer_date,
-		'answerer_name', answerer_name,
-		'helpfulness', answers.helpfulness
-	)) answers
-FROM questions
-INNER JOIN answers
-ON questions.question_id=answers.question_id
-WHERE questions.product_id=1
-GROUP BY questions.question_id
-LIMIT 30;
-
-
-/* Join questions with answers that are joined with photos and return resulting rows as json objects */
-
-
 /* Get single answer as key:answer_id value:JSONobj */
 SELECT JSON_BUILD_OBJECT (
   answer_id,
@@ -55,7 +34,7 @@ FROM (
   WHERE answer_id=5
 ) AS answer_photos;
 
-/* With proper formatting */
+/* With proper formatting DONE */
 SELECT JSON_BUILD_OBJECT (
   'id', answer_id,
   'body', answer_body,
@@ -71,47 +50,6 @@ SELECT JSON_BUILD_OBJECT (
 ) AS answer
 FROM answers
 WHERE answers.answer_id=5;
-
-
-/* Get single question with answers and photos */
-SELECT JSON_BUILD_OBJECT (
-  'question_id', questions.question_id,
-  'question_body', question_body,
-  'question_date', question_date,
-  'asker_name', asker_name,
-  'helpfulness', helpfulness,
-  'reported', reported,
-  'answers', JSON_AGG ((
-    SELECT JSON_BUILD_OBJECT (
-      'id', answer_id,
-      'body', answer_body,
-      'date', answer_date,
-      'answerer_name', answerer_name,
-      'helpfulness', answers.helpfulness,
-      'reported', answers.reported
-    )
-    FROM answers
-    WHERE answers.question_id=questions.question_id
-  ))
-) AS question
-FROM questions
-WHERE questions.question_id=1
-GROUP BY questions.question_id;
-
-SELECT JSON_AGG (ROW_TO_JSON (all_answers))
-FROM (
-  SELECT JSON_BUILD_OBJECT (
-    'id', answer_id,
-    'body', answer_body,
-    'date', answer_date,
-    'answerer_name', answerer_name,
-    'helpfulness', answers.helpfulness,
-    'reported', answers.reported
-  ) answer_id
-  FROM answers
-  WHERE answers.question_id=1
-) AS all_answers;
-
 
 /* THis returns rows of JSON objects */
 SELECT JSON_BUILD_OBJECT (
@@ -169,7 +107,6 @@ SELECT JSON_OBJECT_AGG (answer_id, (
 FROM answers
 WHERE answers.question_id=1;
 
-
 /* This returns a JSON object but maps all the data with each other */
 SELECT JSON_OBJECT_AGG (answer_id, all_answers)
 FROM (
@@ -192,3 +129,49 @@ FROM (
 answers
 WHERE answers.question_id=1;
 
+/* This returns a JSON object with all of the answers (photos included) to a given question. DONE  */
+SELECT JSON_OBJECT_AGG (answer_id, JSON_BUILD_OBJECT (
+  'id', answer_id,
+  'body', answer_body,
+  'date', answer_date,
+  'answerer_name', answerer_name,
+  'helpfulness', helpfulness,
+  'reported', reported,
+  'photos', (
+    SELECT COALESCE(ARRAY_AGG (photo_url), array[]::varchar[])
+    FROM photos
+    WHERE photos.answer_id=answers.answer_id
+  )
+))
+FROM answers
+WHERE answers.question_id=1;
+/* Put this inside the query for questions^^^^ */
+
+/* Get single question with answers and photos DONE */
+SELECT JSON_BUILD_OBJECT (
+  'question_id', questions.question_id,
+  'question_body', question_body,
+  'question_date', question_date,
+  'asker_name', asker_name,
+  'helpfulness', helpfulness,
+  'reported', reported,
+  'answers', (
+    SELECT JSON_OBJECT_AGG (answer_id, JSON_BUILD_OBJECT (
+      'id', answer_id,
+      'body', answer_body,
+      'date', answer_date,
+      'answerer_name', answerer_name,
+      'helpfulness', helpfulness,
+      'reported', reported,
+      'photos', (
+        SELECT COALESCE(ARRAY_AGG (photo_url), array[]::varchar[])
+        FROM photos
+        WHERE photos.answer_id=answers.answer_id
+      )
+    ))
+    FROM answers
+    WHERE answers.question_id=1
+  )
+) AS question
+FROM questions
+WHERE questions.question_id=1;
